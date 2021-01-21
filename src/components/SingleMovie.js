d1a6abc3a1f3808315a945a8459ad96eae548a7c
Kaung Shein <nicholasling1412@gmail.com>
2021-01-21 03:17:57 -0500

    Added content, Readjusted animation, Reformatted Tags

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -13,6 +13,63 @@ const SingleMovie = (props) => {
     }
   };
 
+  const dateString = (e) => {
+    const str = e;
+    const year = str.substring(0, 4);
+    const day = str.substring(8, 10);
+    var month = str.substring(5, 7);
+    switch (month) {
+      case "01":
+        month = "Jan";
+        break;
+      case "02":
+        month = "Feb";
+        break;
+      case "03":
+        month = "Mar";
+        break;
+      case "04":
+        month = "Apr";
+        break;
+      case "05":
+        month = "May";
+        break;
+      case "06":
+        month = "Jun";
+        break;
+      case "07":
+        month = "Jul";
+        break;
+      case "08":
+        month = "Aug";
+        break;
+      case "09":
+        month = "Sep";
+        break;
+      case "10":
+        month = "Oct";
+        break;
+      case "11":
+        month = "Nov";
+        break;
+      case "12":
+        month = "Dec";
+        break;
+      default:
+        month = "";
+    }
+
+    const res = day + " " + month + " " + year;
+    return res;
+  };
+
+  const calculateRuntime = (e) => {
+    const hour = Math.floor(e / 60);
+    const min = e % 60;
+    const concat = hour + "h " + min + "min";
+    return concat;
+  };
+
   const loaded = () => {
     const singleMovieMap = props.moviesListState.map((element, index) => {
       console.log("map", element);
@@ -43,11 +100,27 @@ const SingleMovie = (props) => {
               </div>
               <GenresList genres={element.genres} />
             </div>
-            <div className="movie-active-right">
+            <div className="movie-active-top">
               <div className="movie-active-title">
                 <h1>{element.original_title}</h1>
               </div>
               <div className="movie-active-tagline">{element.tagline}</div>
+            </div>
+            <div className="movie-active-right">
+              <div className="movie-active-subtitle">
+                <span className="vote-average">{element.vote_average}</span>
+                <span className="dot">•</span>
+                <span className="date">{dateString(element.release_date)}</span>
+                <span className="dot">•</span>
+                <span className="country">
+                  {element.production_countries[0].iso_3166_1}
+                </span>
+                <span className="dot">•</span>
+                <span className="runtime">
+                  {calculateRuntime(element.runtime)}
+                </span>
+              </div>
+              <div className="movie-active-overview">{element.overview}</div>
             </div>
           </div>
         </div>
